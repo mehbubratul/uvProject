babe8a1c7da8acfda7f3cbd87f175f1a2664f32a
mehbubratul <ratulewu@gmail.com>
2022-09-21 14:24:26 +0600

    modification in FileService Test, PerformanceMeasurement Test, Remore ScanningService Test

--- src/test/java/org/ulventech/FileServiceTest.java
+++ src/test/java/org/ulventech/FileServiceTest.java
@@ -1,10 +1,17 @@
 package org.ulventech;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileServiceTest {
+
+    @BeforeEach
+    void setUp(){
+    }
 
     //region valid test cases
     @Test
@@ -39,9 +46,14 @@ class FileServiceTest {
 
     @Test
     void systemWillGenerateValidOutFilePath(){
-        String expectedFilePath = "E:\\git\\uvProject\\src\\main\\resources\\uvTech.txt";
+
+        String expectedLocation = new StringBuilder(Paths.get("").toAbsolutePath().toString())
+                .append("\\src\\main\\resources\\")
+                .append("uvTech.txt")
+                .toString();
+
         String actualFilePath = FileService.getOutFilePath();
-        assertEquals(expectedFilePath,actualFilePath);
+        assertEquals(expectedLocation,actualFilePath);
     }
     //endregion
 }
--- src/test/java/org/ulventech/PerformanceMeasurementServiceTest.java
+++ src/test/java/org/ulventech/PerformanceMeasurementServiceTest.java
@@ -4,10 +4,59 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Since, the last two test cases are taking max accepted number as input,
+ * It's recommended to have large memory space (100 GB for each test case).
+ * Moreover, both of the test cases will require enough time ( 20 minutes around , in my system configuration).
+ */
 class PerformanceMeasurementServiceTest {
 
     private static final int MAX_INT_ACCEPTED_VALUE = (int) (Math.pow(2, 30) - 1);
 
+    @Test
+    void givenMinAcceptedIntValueAsInputNumberCanPerformIn10Seconds_ReturnTrue() {
+        // given
+
+        int inputNumber = 1;
+
+        // when
+        long expectedTimeElapsedInSeconds = 10;
+        // then
+        PerformanceMeasurementService.initializationOfPerformanceMeasurement();
+
+        FileService.writeContentToFile(inputNumber, FileService.getOutFilePath());
+
+        long timeElapsedInSecond = PerformanceMeasurementService.endMeasuringTimePerformance();
+
+        assertTrue(timeElapsedInSecond <= expectedTimeElapsedInSeconds);
+    }
+
+    @Test
+    void givenMinAcceptedIntValueAsInputNumberCanPerformUsing3MBOfMemory_ReturnTrue() {
+        // given
+        int inputNumber = 1;
+
+        // when
+        long expectedMemoryInMegabytes = 3;
+
+        // then
+        PerformanceMeasurementService.initializationOfPerformanceMeasurement();
+
+        FileService.writeContentToFile(inputNumber, FileService.getOutFilePath());
+
+        long usedMemoryInMegabytes = PerformanceMeasurementService.endMeasuringMemoryPerformance();
+
+        assertTrue(expectedMemoryInMegabytes >= usedMemoryInMegabytes);
+    }
+
+
+    /**
+     * This test case will take 1073741823 as input number;
+     * This is max accepted number according to the problem statement
+     * This method will print 1073741823 numbers of line in file; where each line is 100 char length.
+     * The file size will be more than 100 GB.
+     * Alert : This will take long execution time ( in my os, it takes around than 20 minutes most of the time)
+     */
     @Test
     void givenMaxAcceptedIntValueAsInputNumberCanPerformUsing10MBOfMemory_ReturnTrue() {
         // given
@@ -26,6 +75,13 @@ class PerformanceMeasurementServiceTest {
         assertTrue(expectedMemoryInMegabytes >= usedMemoryInMegabytes);
     }
 
+    /**
+     * This test case will take 1073741823 as input number;
+     * This is Max accepted number according to the problem statement
+     * This method will print 1073741823 numbers of line in file; where each line is 100 char length.
+     * The file size will be more than 100 GB.
+     * Alert : This will take long execution time ( in my os, it takes around than 20 minutes most of the time)
+     */
     @Test
     void givenMaxAcceptedIntValueAsInputNumberCanPerformIn1500Seconds_ReturnTrue() {
         // given
@@ -43,5 +99,4 @@ class PerformanceMeasurementServiceTest {
 
         assertTrue(timeElapsedInSecond <= expectedTimeElapsedInSeconds);
     }
-
 }
